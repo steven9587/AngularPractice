b08ba730b84d0db39571cbc09141a7fef85f78c1
steven9587 <steven9587@gmail.com>
2023-10-11 14:28:31 +0800

    跨域呼叫API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,56 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { TodoData, TodoListStatus } from './model/todo.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title :string = 'todoThing';
   placeHolder : string = "What needs to be done???"
   toggleAllStatus : boolean = false;
   nowTodoListStatus = TodoListStatus.All;
   todoListStatus = TodoListStatus;
+  keyINInputText = '';
 
-  todoDataList : TodoData[] = [
-    {
-      thing : '代辦事項1',
-      status : false,
-      editing : false
-    } , {
-      thing : '代辦事項2',
-      status : true,
-      editing : false
-    } , {
-      thing : '代辦事項3',
-      status : false,
-      editing : false
-    }
-  ].map(data => new TodoData(data.thing, data.status, data.editing));
+  // todoDataList : TodoData[] = [
+  //   {
+  //     thing : '代辦事項1',
+  //     status : false,
+  //     editing : false
+  //   } , {
+  //     thing : '代辦事項2',
+  //     status : true,
+  //     editing : false
+  //   } , {
+  //     thing : '代辦事項3',
+  //     status : false,
+  //     editing : false
+  //   }
+  // ].map(data => new TodoData(data.thing, data.status, data.editing));
+  todoDataList : TodoData[] = [];
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    // 字串位置可以放檔案位置或api網址
+    this.http.get<TodoData[]>('/api/todo2_16').subscribe(data => {
+      this.todoDataList = data;
+    });
+  }
+
+
 
   toggleAll() {
     this.toggleAllStatus = !this.toggleAllStatus;
     this.todoDataList.forEach( item => {
-      item.status = this.toggleAllStatus;
+      item.Status = this.toggleAllStatus;
     })
   }
 
   check(item: TodoData) {
-    item.status = !item.status;
+    item.Status = !item.Status;
     this.toggleAllStatus = this.getCompletedTodoList().length == this.todoDataList.length ? true : false;
   }
 
@@ -45,9 +58,9 @@ export class AppComponent {
     this.todoDataList = this.todoDataList.filter(data => data != item);
   }
 
-  enterTodo(inputText: HTMLInputElement) {
-    this.todoDataList.push(new TodoData(inputText.value, false, false))
-    inputText.value = '';
+  enterTodo() {
+    this.todoDataList.push(new TodoData(false, "00000000-0000-0000-0000-000000000000", this.keyINInputText, false, "2023-10-11T00:00:00"))
+    this.keyINInputText = '';
   }
 
   editing(item: TodoData) {
@@ -55,7 +68,7 @@ export class AppComponent {
   }
 
   saveEditInput(item: TodoData, editInput: HTMLInputElement) {
-    item.thing = editInput.value;
+    item.Thing = editInput.value;
     item.closeEditing();
   }
 
@@ -83,14 +96,14 @@ export class AppComponent {
   }
 
   getActiveTodoList(): TodoData[] {
-    return this.todoDataList.filter(data => !data.status);
+    return this.todoDataList.filter(data => !data.Status);
   }
 
   getCompletedTodoList(): TodoData[] {
-    return this.todoDataList.filter(data => data.status);
+    return this.todoDataList.filter(data => data.Status);
   }
 
   clearCompleted() {
-    this.todoDataList = this.todoDataList.filter(data => !data.status);
+    this.todoDataList = this.todoDataList.filter(data => !data.Status);
   }
 }
--- src/app/model/todo.model.ts
+++ src/app/model/todo.model.ts
@@ -1,20 +1,27 @@
 export class TodoData {
-    thing: string;
-    status: boolean;
-    editing: boolean;
+    // thing: string;
+    // status: boolean;
+    // editing: boolean;
+    Editing: boolean;
+    TodoId: string;
+    Thing: string;
+    Status: boolean;
+    CreateTime: string;
 
-    constructor(_thing : string,_status: boolean,_editing: boolean) {
-        this.thing = _thing;
-        this.status = _status;
-        this.editing = _editing;
+    constructor(_Editing : boolean,_TodoId: string,_Thing: string,_Status: boolean,_CreateTime: string) {
+        this.Editing = _Editing;
+        this.TodoId = _TodoId;
+        this.Thing = _Thing;
+        this.Status = _Status;
+        this.CreateTime = _CreateTime;
     }
 
     openEditing() {
-        this.editing = true;
+        this.Editing = true;
     }
 
     closeEditing() {
-        this.editing = false;
+        this.Editing = false;
     }
 
 }
